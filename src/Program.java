f18f42d7aa1415bcf95113bdd6c4b02b42f469be
Shutao Xie <xie_shutao@hotmail.com>
2015-10-17 19:37:11 -0400

    scanner

--- src/Program.java
+++ src/Program.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Program {
 	//modifier should be in front of String
@@ -6,19 +7,29 @@ public class Program {
 	
 	public static Person person;//reference to the Person class
 	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Please enter your name ");
+		String myName = input.nextLine();
+		
+		System.out.print("Please enter your gender: ");
+		String myGender = input.nextLine();
 
+		System.out.print("Please enter your age: ");
+		int myAge = input.nextInt();
+		
       System.out.println("Program Started...");
       //_name="Shutao";
       //System.out.println(_name);
       
-      person = new Person(47);
-
+      person = new Person(myAge);
       
       //person.gender = "Male";
       //person.name = "Tom";
       
-      person.setName("Shutao");
-      person.setGender("Male");
+      person.setName(myName);
+      person.setGender(myGender);
 
       System.out.println(person.getName());
       
@@ -27,5 +38,6 @@ public class Program {
       System.out.printf("%s, a %s, is %d years old.", person.getName(),person.getGender(),person.getAge());
       
 	}
+
 
 }
