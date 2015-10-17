335814e8e51a4311fbc732784015e6dd8f1c1511
Shutao Xie <xie_shutao@hotmail.com>
2015-10-17 19:05:57 -0400

    set, get, constructor, instance variable and a lot of info

--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,31 @@
 
 public class Program {
-	//put something before String
+	//modifier should be in front of String
 	//name is an instance variable
-private static String _name = "";
+    //private static String _name = "";
+	
+	public static Person person;//reference to the Person class
 	public static void main(String[] args) {
 
-      System.out.println("Program Started from ");
-      _name="Shutao";
-      System.out.println(_name);
+      System.out.println("Program Started...");
+      //_name="Shutao";
+      //System.out.println(_name);
+      
+      person = new Person(47);
+
+      
+      //person.gender = "Male";
+      //person.name = "Tom";
+      
+      person.setName("Shutao");
+      person.setGender("Male");
+
+      System.out.println(person.getName());
+      
+      person.runs();
+      
+      System.out.printf("%s, a %s, is %d years old.", person.getName(),person.getGender(),person.getAge());
+      
 	}
 
 }
